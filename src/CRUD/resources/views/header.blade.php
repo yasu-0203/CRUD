6a7657a9f2881694f0f79bc1a6d71307467f4a17
RyotaAbe1014 <bm1000bmmm@gmail.com>
2021-12-13 13:36:21 +0900

    [add] ログインしていないユーザーにはアバターのアイコンは表示されないようにする記述を追加

--- src/CRUD/resources/views/header.blade.php
+++ src/CRUD/resources/views/header.blade.php
@@ -17,8 +17,9 @@
       </div>
     </div>
     <ul class="navbar-nav">
+      @auth
       <!-- Avatar -->
-      {{-- 認証を使う際、アバターに関してはログインしていない時は時はアバターは表示されず、登録画面に遷移する記述を追加 --}}
+      {{-- 認証を使う際、アバターに関してはログインしていない時は時はアバターは表示されず、登録画面に遷移する記述を追加してほしい(sign inや会員登録など) --}}
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarDropdownMenuLink" role="button"
           data-mdb-toggle="dropdown" aria-expanded="false">
@@ -30,6 +31,7 @@
             <a class="dropdown-item" href="#">My profile</a>
           </li>
           <li>
+            {{-- onclickの後はスクリプト文なのでここの処理をresources/js/app.jsに移行し、Jqueryの記述にする必要がある --}}
             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
             document.getElementById('logout-form').submit();">
               {{ __('Logout') }}
@@ -40,6 +42,7 @@
           </li>
         </ul>
       </li>
+      @endauth
     </ul>
   </nav>
   <!-- Navbar -->
--- src/CRUD/resources/views/welcome.blade.php
+++ src/CRUD/resources/views/welcome.blade.php
@@ -2,12 +2,13 @@
 @section('content')
 <!-- Background image -->
 <div class="p-5 text-center bg-image" style="
-          background-image: url('https://pixabay.com/get/g0edf9a7dadce2d79b95547cde6944529f43e16a17aa75a38d34bfbe156df726546721cc3ae63de414df0a5458fdf3ce48b8622b6d8d1f3b3aa31383d490327ac7f1b9765b917e268618c25ee54ac155d_1920.jpg');
+          background-image: url('');
           height: 1000px;
         ">
     <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
         <div class="d-flex justify-content-center align-items-center h-100">
             <div class="text-white animate__animated animate__backInDown">
+                {{-- 見出しとテキストになるので考えよう(後回しでOK) --}}
                 <h1 class="mb-3">Heading</h1>
                 <h4 class="mb-3">Subheading</h4>
                 {{-- ログインしていない場合ははSTARTを押した際、登録画面に遷移するようにしたい --}}
