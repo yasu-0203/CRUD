18b6ccc051a3328702cb7b804e480ae2de61c047
RyotaAbe1014 <bm1000bmmm@gmail.com>
2021-12-15 20:45:28 +0900

    [add] ユーザー登録画面を編集

--- src/CRUD/resources/views/auth/login.blade.php
+++ src/CRUD/resources/views/auth/login.blade.php
@@ -76,25 +76,35 @@
         </div>
     </div>
 </div> --}}
-<div class="row d-flex justify-content-center align-items-center">
+<div class="row d-flex justify-content-center align-items-center animate__animated animate__fadeIn">
     <div class="col-sm-6">
-        <div class="card text-center border border-primary shadow-0" style="background-color:#ffffff;">
+        <div class="card text-center border shadow-4" style="background-color:#ffffff;">
             {{-- Form --}}
             <form method="POST" action="{{ route('login') }}">
                 @csrf
                 <h4 class="card-title">Login</h4>
+                {{-- error Message --}}
+                <div class="card  text-white mb-4 col-8 mx-auto" style="background-color:#fda5b2;">
+                    <div class="card-body">
+                        <p class="card-text">
+                            sample sample sample sample sample sample sample sample sample sample sample sample sample
+                        </p>
+                    </div>
+                </div>
+                {{-- error Message --}}
                 <!-- Email入力-->
                 <div class="form-outline mb-4 col-8 mx-auto">
-                    <input type="email" class="form-control" name="email" value="{{ old('email') }}"/>
+                    <input type="email" class="form-control" name="email" value="{{ old('email') }}" />
                     <label class="form-label" for="form1Example1">Email address</label>
                 </div>
                 <!-- Password入力-->
                 <div class="form-outline mb-4 col-8 mx-auto">
-                    <input type="password" class="form-control" name="password"/>
+                    <input type="password" class="form-control" name="password" />
                     <label class="form-label" for="form1Example2">Password</label>
                 </div>
                 {{-- loginボタン --}}
-                <button type="submit" class="btn btn-outline-primary btn-rounded mb-4 " data-mdb-ripple-color="#000000"> Login
+                <button type="submit" class="btn btn-outline-primary btn-rounded mb-4 " data-mdb-ripple-color="#000000">
+                    Login
                 </button>
             </form>
         </div>
--- src/CRUD/resources/views/auth/register.blade.php
+++ src/CRUD/resources/views/auth/register.blade.php
@@ -1,76 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<div class="row d-flex justify-content-center align-items-center animate__animated animate__fadeIn">
+    <div class="col-sm-6">
+        <div class="card text-center border shadow-4" style="background-color:#ffffff;">
+            {{-- Form --}}
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+                <h4 class="card-title">Login</h4>
+                {{-- error Message --}}
+                <div class="card  text-white mb-4 col-8 mx-auto" style="background-color:#fda5b2;">
+                    <div class="card-body">
+                        <p class="card-text">
+                            sample sample sample sample sample sample sample sample sample sample sample sample sample
+                        </p>
+                    </div>
                 </div>
-            </div>
+                {{-- error Message --}}
+                <!-- Email入力-->
+                <div class="form-outline mb-4 col-8 mx-auto">
+                    <input type="name" class="form-control" name="name" value="{{ old('name') }}" />
+                    <label class="form-label" for="form1Example1">Your Name</label>
+                </div>
+                <div class="form-outline mb-4 col-8 mx-auto">
+                    <input type="email" class="form-control" name="email" value="{{ old('email') }}" />
+                    <label class="form-label" for="form1Example1">Email address</label>
+                </div>
+                <!-- Password入力-->
+                <div class="form-outline mb-4 col-8 mx-auto">
+                    <input type="password" class="form-control" name="password" autocomplete="new-password" />
+                    <label class="form-label" for="form1Example2">Password</label>
+                </div>
+                <div class="form-outline mb-4 col-8 mx-auto">
+                    <input type="password-confirm" class="form-control" name="password_confirmation"
+                        autocomplete="new-password" />
+                    <label class="form-label" for="form1Example2">Confirm
+                        Password</label>
+                </div>
+                {{-- loginボタン --}}
+                <button type="submit" class="btn btn-outline-primary btn-rounded mb-4 " data-mdb-ripple-color="#000000">
+                    Register
+                </button>
+            </form>
         </div>
     </div>
 </div>
